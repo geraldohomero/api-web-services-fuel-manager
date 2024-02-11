6b208882e53b819a602c1ba9a844552fc2cf88a7
geraldohomero <geraldohomero@protonmail.com>
2024-02-11 15:45:57 -0300

    add Modelos de consumo e veículos

---
+++ api-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_web_services_fuel_manager.Models;
+
+//da nome para tabelas
+[Table("Consumos")]
+public class Consumo
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Descricao { get; set; }
+    [Required]
+    public DateTime Data { get; set; }
+    [Required]
+    public decimal Valor { get; set; }
+    [Required]
+    public TipoCombustivel Tipo { get; set; }
+    
+    // um veículo tem n consumos, um relacionamento de 1 para n
+    // notação do ef 
+    [Required]
+    public int VeiculoId { get; set; } //ForeignKey
+    
+    // navegação virtualk
+    // o ef quando for carregar as informações do consumo
+    // ele pode carregar todas as informações do veículo
+    // associado a esse consumo. Navegar (de uma entidade para outra)
+    // de forma mais simples.
+    public Veiculo Veiculo { get; set; } // navegação virtual
+}
+
+public enum TipoCombustivel
+{
+    Diesel,
+    Etanol,
+    Gasolina
+}
