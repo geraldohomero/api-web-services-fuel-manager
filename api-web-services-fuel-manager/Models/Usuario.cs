35603e88adc89a6cb60c27eb5cebe4edb0168bd6
Geraldo Homero <geraldohomero@protonmail.com>
2024-02-13 17:52:49 -0300

    Criando VeiculoUsuarios

---
+++ api-web-services-fuel-manager/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace api_web_services_fuel_manager.Models
+{
+    [Table("Usuários")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public int Perfil { get; set; }
+
+    }
+    public enum Perfil
+    {
+        [Display(Name = "Administrador")]
+        Administrador,
+        [Display(Name = "Usuário")]
+        Usuario
+    }
+}
